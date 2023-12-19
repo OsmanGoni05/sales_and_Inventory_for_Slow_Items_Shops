8090e4daf9790553f442648300a013327bb86aee
Osman Goni <osmangoni9605@gmail.com>
2023-12-19 23:43:41 +0600

    Feature : Added InventorySummaryController

--- Controllers/InventorySummaryController.cs
+++ Controllers/InventorySummaryController.cs
@@ -1,6 +1,8 @@
+using System.Linq.Expressions;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using sales_and_Inventory_for_Slow_Items_Shops.Constants;
 using sales_and_Inventory_for_Slow_Items_Shops.data;
 using sales_and_Inventory_for_Slow_Items_Shops.models;
@@ -10,11 +12,11 @@ namespace sales_and_Inventory_for_Slow_Items_Shops.Controllers;
 [ApiController]
 [Route("[controller]")]
 
-public class InventroySummaryController : ControllerBase
+public class InventorySummaryController : ControllerBase
 {
-    public readonly ApplicationDbContext _context;
     public readonly IMapper _mapper;
-    public InventroySummaryController(ApplicationDbContext context, IMapper mapper)
+    public readonly ApplicationDbContext _context;
+    public InventorySummaryController(ApplicationDbContext context, IMapper mapper)
     {
         _mapper = mapper;
         _context = context;
@@ -22,43 +24,75 @@ public class InventroySummaryController : ControllerBase
     [HttpGet("GetById")]
     public IActionResult GetById(int id)
     {
-        var find = _context.InventorySummaries.Find(id);
-        InventorySummary inventorySummary = _mapper.Map<InventorySummary>(find);
-        return Ok(inventorySummary);
+        var find = _context.InventorySummaries.Include(element => element.Product)
+        .Where(element => element.Id == id)
+        .Select(element => new
+        {
+            element.Id,
+            element.ProductId,
+            element.Product.ProductType.ProductName,
+            element.ProductQuantity,
+            element.TotalPrice,
+            element.CreatedAt,
+            element.CreatedBy,
+            element.UpdatedAt,
+            element.UpdatedBy
+        }).FirstOrDefault();
+
+        return Ok(find);
     }
-    [HttpGet("Get")]
-    public IActionResult Get()
+
+    [HttpGet("Filter")]
+    public IActionResult Filter([FromQuery] InventoryFilterSummaryRequest request)
     {
-        List<InventorySummary> inventorySummaries = _context.InventorySummaries.ToList();
-        List<InventorySummaryResponse> inventoryResponses = _mapper.Map<List<InventorySummaryResponse>>(inventorySummaries);
-        return Ok(inventoryResponses);
-    }
-    [HttpPost("Post")]
-    public IActionResult Post(InventorySummaryRequest inventorySummaryRequest)
-    {
-        InventorySummary inventorySummary = _mapper.Map<InventorySummary>(inventorySummaryRequest);
-        _context.InventorySummaries.Add(inventorySummary);
-        var result = _context.SaveChanges();
-        return Ok(ResponseMessage.SUCCESS_MESSAGE);
-    }
-    [HttpPut("Put")]
-    public IActionResult Update(int id, InventorySummaryRequest inventorySummaryRequest)
-    {
-        InventorySummary? inventorySummary = _context.InventorySummaries.Find(id);
-        if(inventorySummary is null) return BadRequest(ResponseMessage.NOT_FOUND);
-        inventorySummary = _mapper.Map(inventorySummaryRequest, inventorySummary);
-        inventorySummary.UpdatedAt = DateTime.UtcNow;
-        inventorySummary.UpdatedBy = 0;
-        _context.InventorySummaries.Update(inventorySummary);
-        var result = _context.SaveChanges();
-        return Ok(ResponseMessage.SUCCESS_MESSAGE);
-    }
-    [HttpDelete("Delete")]
-    public IActionResult Delete(int id)
-    {
-        var find = _context.InventorySummaries.Find(id);
-        _context.InventorySummaries.Remove(find);
-        var result = _context.SaveChanges();
+        request.Page = request.Page == 0 ? 1 : request.Page;
+        var query = _context.InventorySummaries.AsQueryable();
+
+         if (!request.ProductId.ToString().IsNullOrEmpty())
+         {
+           query= query.Where(element => element.ProductId == request.ProductId);
+         }//if
+
+        List<dynamic> elements = query
+            .OrderByDescending(element => element.Id)
+            .Skip((request.Page - 1) * request.Take)
+            .Take(request.Take)
+            .Select(element => new
+        {
+            element.Id,
+            element.ProductId,
+            element.Product.ProductType.ProductName,
+            element.ProductQuantity,
+            element.TotalPrice,
+            element.CreatedAt,
+            element.CreatedBy,
+            element.UpdatedAt,
+            element.UpdatedBy
+        }).ToList<dynamic>();
+
+        int count = query.Count();
+
+        int totalPage = count <= request.Take ? 1 : (count / (request.Take == 0 ? 1 : request.Take));
+
+
+        var result = new BaseFilterResponse
+        {
+            Data = elements,
+            totalElements = count,
+            Page = request.Page,
+            Take = request.Take,
+            TotalPage = totalPage
+        };
         return Ok(result);
+    }
+
+
+    [HttpPost("Post")]
+    public IActionResult Post(InventorySummaryRequest request)
+    {
+        InventorySummary element = _mapper.Map<InventorySummary>(request);
+        _context.InventorySummaries.Add(element);
+        var result = _context.SaveChanges();
+        return Ok(ResponseMessage.SUCCESS_MESSAGE);
     }
 }
